d0d1f1d55cc3fa5f845ea216e8e81cc8ea5feef7
Ize Imai <izeimai@gmail.com>
2019-02-23 18:29:23 -0500

    Added file structure

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Basic burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
