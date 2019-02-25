a43a8267937336bea7abd9ced1d2e2cf970ca114
Ize Imai <izeimai@gmail.com>
2019-02-24 22:15:46 -0500

    added .env file back but empty

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,21 @@
 var mysql = require("mysql");
 
 //stored password in separate env file so need dotenv if running locally but this is for heroku so need to remove
-if (process.env.NODE_ENV !== 'production') {
-    require("dotenv").config();
-}
+require('dotenv').config()
+var connection;
 
-// create connection variable
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 
 // Connect
 connection.connect(function (err) {
