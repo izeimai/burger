4ccf585bce68c15bfa24a24f729f13b48ef83da2
Ize Imai <izeimai@gmail.com>
2019-02-24 21:25:01 -0500

    Added aws-sdk package

--- config/connection.js
+++ config/connection.js
@@ -2,25 +2,31 @@
 var mysql = require("mysql");
 
 //stored password in separate env file so need dotenv if running locally but this is for heroku so need to remove
-//require("dotenv").config();
+
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+    accessKeyId: process.env.S3_KEY,
+    secretAccessKey: process.env.S3_SECRET
+});
 
 // create connection variable
 var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: process.env.DB_PASS,
-        database: "burgers_db"
-    });
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.S3_SECRET,
+    database: "burgers_db"
+});
 
-    // Connect
-    connection.connect(function (err) {
-        if (err) {
-            console.error("error connecting: " + err.stack);
-            return;
-        }
-        console.log("connected as id " + connection.threadId);
-    });
+// Connect
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-    // Export connection for use with ORM
-    module.exports = connection;
+// Export connection for use with ORM
+module.exports = connection;
