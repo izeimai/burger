75e2431b28ec55918c7ba76290a44aa4f3727977
Ize Imai <izeimai@gmail.com>
2019-02-24 21:28:06 -0500

    Changed key name

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,7 @@ var mysql = require("mysql");
 const aws = require('aws-sdk');
 
 let s3 = new aws.S3({
-    accessKeyId: process.env.S3_KEY,
-    secretAccessKey: process.env.S3_SECRET
+    secretAccessKey: process.env.DB_PASS
 });
 
 // create connection variable
@@ -15,7 +14,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.S3_SECRET,
+    password: s3.secretAccessKey,
     database: "burgers_db"
 });
 
